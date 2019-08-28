15a4457e704c0e05fb34e01f4ee9f346dbf5382d
mike.spolarich <mike.spolarich@blend.com>
2019-08-28 12:05:29 -0700

    No operations on resolved stuff

--- db/transaction.go
+++ db/transaction.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/blend/go-sdk/ex"
 	"strconv"
+	"sync"
 )
 
 // ErrInnerTxExists is an error that occurs when you try to Begin multiple transactions inside of the same parent
@@ -12,25 +13,32 @@ import (
 // parent can have no more than one inner transaction
 const ErrInnerTxExists ex.Class = "db: inner transaction already exists"
 
+
 // Tx is a database transaction
 type Tx struct {
 	*sql.Tx
 
-	savePoint int
-	nested    *Tx
-	resolved  bool
+	savePoint     int
+	nested        *Tx
+	resolved      bool
+	resolutionMux sync.Mutex
 }
 
 // Begin starts a new nested transaction
 func (tx *Tx) Begin() (*Tx, error) {
+	tx.resolutionMux.Lock()
+	defer tx.resolutionMux.Unlock()
 	if tx.nested != nil && !tx.nested.resolved {
 		return nil, ex.New(ErrInnerTxExists)
 	}
+	if tx.savePoint > 0 && tx.resolved {
+		return nil, sql.ErrTxDone
+	}
+
 	tx.nested = &Tx{
 		Tx:        tx.Tx,
 		savePoint: tx.savePoint + 1,
 	}
-
 	_, err := tx.Exec(fmt.Sprintf("SAVEPOINT PT%s", strconv.Itoa(tx.nested.savePoint)))
 
 	if err != nil {
@@ -42,7 +50,12 @@ func (tx *Tx) Begin() (*Tx, error) {
 
 // Rollback rolls back the transaction. If this is an inner transaction it rolls back to the last save point
 func (tx *Tx) Rollback() error {
-	tx.resolved = true
+	tx.resolutionMux.Lock()
+	defer tx.resolutionMux.Unlock()
+	if tx.resolved {
+		return sql.ErrTxDone
+	}
+	tx.markResolved()
 
 	if tx.savePoint > 0 {
 		_, err := tx.Exec(fmt.Sprintf("ROLLBACK TO SAVEPOINT PT%s", strconv.Itoa(tx.savePoint)))
@@ -52,22 +65,43 @@ func (tx *Tx) Rollback() error {
 	return tx.Tx.Rollback()
 }
 
+func (tx *Tx) markResolved() {
+	if tx.nested != nil {
+		tx.nested.resolutionMux.Lock()
+		tx.nested.markResolved()
+		tx.nested.resolutionMux.Unlock()
+	}
+	tx.resolved = true
+}
+
 // Commit commits the transaction. If this is an inner transaction it releases the last save point.
 func (tx *Tx) Commit() error {
+	tx.resolutionMux.Lock()
+	defer tx.resolutionMux.Unlock()
+
+	// If the client is trying to re-commit a transaction thats resolved, error
+	if tx.resolved {
+		return sql.ErrTxDone
+	}
+
+	return tx.innerCommit()
+}
+
+func (tx *Tx) innerCommit() error {
 	if tx.nested != nil && !tx.nested.resolved {
-		err := tx.nested.Commit()
+		tx.resolved = true
+		err := tx.nested.innerCommit()
 		if err != nil {
 			return err
 		}
 	}
 
-	tx.resolved = true
-
-	if tx.savePoint > 0 {
+	if tx.savePoint > 0 && !tx.resolved {
+		tx.resolved = true
 		_, err := tx.Exec(fmt.Sprintf("RELEASE SAVEPOINT PT%s", strconv.Itoa(tx.savePoint)))
-
 		return err
 	}
 
+	// This is a "root" transaction commit it
 	return tx.Tx.Commit()
 }
--- db/transaction_test.go
+++ db/transaction_test.go
@@ -1,11 +1,12 @@
 package db
 
 import (
+	"database/sql"
 	"github.com/blend/go-sdk/assert"
 	"testing"
 )
 
-func TestTransactionSingleCheckpointRollback(t *testing.T) {
+func TestTransactionInnerRollback(t *testing.T) {
 	assert := assert.New(t)
 
 	tx, err := defaultDB().Begin()
@@ -30,6 +31,12 @@ func TestTransactionSingleCheckpointRollback(t *testing.T) {
 	assert.Nil(err)
 	assert.True(found)
 
+
+	illegal, err := tx.Begin()
+	assert.NotNil(err)
+	assert.Nil(illegal)
+	assert.Equal(ErrInnerTxExists, err.Error())
+
 	err = inner.Rollback()
 	assert.Nil(err)
 
@@ -37,4 +44,62 @@ func TestTransactionSingleCheckpointRollback(t *testing.T) {
 	found, err = defaultDB().Invoke(OptTx(tx)).Query("Select 1 from unique_obj where id = $1", 2).Any()
 	assert.Nil(err)
 	assert.False(found)
+
+	// Cant roll back resolved tx
+	err = inner.Rollback()
+	assert.Equal(sql.ErrTxDone, err)
+}
+
+func TestTransactionOuterRollback(t *testing.T) {
+	assert := assert.New(t)
+
+	tx, err := defaultDB().Begin()
+	assert.Nil(err)
+	defer tx.Rollback()
+
+	assert.Nil(IgnoreExecResult(defaultDB().Invoke(OptTx(tx)).Exec("CREATE TABLE IF NOT EXISTS unique_obj (id int not null primary key, name varchar)")))
+
+	outer, err := tx.Begin()
+	assert.Nil(err)
+
+	assert.Nil(defaultDB().Invoke(OptTx(outer)).Create(&uniqueObj{ID: 1, Name: "one"}))
+	var verify uniqueObj
+	assert.Nil(secondArgErr(defaultDB().Invoke(OptTx(outer)).Get(&verify, 1)))
+
+	inner, err := outer.Begin()
+	assert.Nil(err)
+	defaultDB().Invoke(OptTx(inner)).Create(&uniqueObj{ID: 2, Name: "two"})
+
+	// Available on all
+	found, err := defaultDB().Invoke(OptTx(inner)).Query("Select 1 from unique_obj where id = $1", 2).Any()
+	assert.Nil(err)
+	assert.True(found)
+
+	found, err = defaultDB().Invoke(OptTx(outer)).Query("Select 1 from unique_obj where id = $1", 2).Any()
+	assert.Nil(err)
+	assert.True(found)
+
+	found, err = defaultDB().Invoke(OptTx(tx)).Query("Select 1 from unique_obj where id = $1", 2).Any()
+	assert.Nil(err)
+	assert.True(found)
+
+	err = outer.Rollback()
+	assert.Nil(err)
+
+	// Outer can't be committed after rollback
+	err = outer.Commit()
+	assert.Equal(sql.ErrTxDone, err)
+
+	// Not available on tx
+	found, err = defaultDB().Invoke(OptTx(tx)).Query("Select 1 from unique_obj where id = $1", 2).Any()
+	assert.Nil(err)
+	assert.False(found)
+
+	// Inner can't be rolled back
+	err = inner.Rollback()
+	assert.Equal(sql.ErrTxDone, err)
+
+	// Inner can't be committed back
+	err = inner.Commit()
+	assert.Equal(sql.ErrTxDone, err)
 }
